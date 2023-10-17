fe46d5e3f38098bc4395c9b3bac40bfc94092647
Romer Jasen Jimenez <businessmailromer@gmail.com>
2023-10-17 17:18:18 +0800

    Second Commit

---
+++ Qst/js/app.js
@@ -0,0 +1,10 @@
+data = [
+    {question: "The plaka is the oldest quarter of which city", 
+    answers: {
+        a: "Athens",
+        b: "Praque", 
+        c: "Rome", 
+        d: "Vienna"}, 
+        correctAnswer: "a"},
+    ];
+    
