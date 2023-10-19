06164dbc0fa6b97bca7bfc8530aaffbec0eca912
Romer Jasen Jimenez <businessmailromer@gmail.com>
2023-10-19 08:19:58 +0800

    third commit

--- Qst/js/app.js
+++ Qst/js/app.js
@@ -1,10 +1,53 @@
-data = [
-    {question: "The plaka is the oldest quarter of which city", 
-    answers: {
-        a: "Athens",
-        b: "Praque", 
-        c: "Rome", 
-        d: "Vienna"}, 
-        correctAnswer: "a"},
-    ];
+// const quizBox = document.getElementById('quiz');
+// const resultsBox = document.getElementById('results');
+// const submitButton = document.getElementById('submit');
+
+
+// function buildQuiz(){}
+// function showResults(){}
+// buildQuiz();
+// submitButton.addEventListener('click', showResults);
+
+
+// const myQuestions = [];
+// for(i=0; i < data.length; i++){
+// myQuestions.push(data[i]);
+// }
+
+
+function buildQuiz(){
+    const output = [];
+    myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+    const answers = [];
+    for(letter in currentQuestion.answers){
+    answers.push(
+    `<label><input type="radio" name="question${questionNumber}"
+    value="${letter}" class="rad_butn">
+    ${letter} :
+    ${currentQuestion.answers[letter]}
+    </label>`
+    );
+    }
+    output.push(
+    `<div class="slide">
+    <div class="question"> ${(questionNumber+1)}.
+    ${currentQuestion.question} </div>
+    <div class="answers"> ${answers.join("")} </div>
+    </div>`
+    );
+    }
+    );
+    quizBox.innerHTML = output.join('');
+    }
     
+function showResults(){}
+const quizBox = document.getElementById('quiz');
+const resultsBox = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [];
+for(i=0; i < data.length; i++){
+myQuestions.push(data[i]);
+}
+buildQuiz();
+submitButton.addEventListener('click', showResults);
